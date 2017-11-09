3ab1673c079947bc41915e62ea05405af1146436
Kevin Hellemun <khellemun@bunq.com>
2017-11-09 14:21:08 +0100

    Added DraftPayment to NotificatoinUrl test. #67

--- tests/Model/Generated/Object/NotificationUrlTest.php
+++ tests/Model/Generated/Object/NotificationUrlTest.php
@@ -5,6 +5,8 @@ namespace bunq\test\Model\Object;
 use bunq\Exception\BunqException;
 use bunq\Model\Core\BunqModel;
 use bunq\Model\Generated\Endpoint\BunqMeTab;
+use bunq\Model\Generated\Endpoint\ChatMessage;
+use bunq\Model\Generated\Endpoint\DraftPayment;
 use bunq\Model\Generated\Endpoint\Payment;
 use bunq\Model\Generated\Object\NotificationUrl;
 use bunq\test\BunqSdkTestBase;
@@ -16,6 +18,14 @@ use bunq\Util\FileUtil;
  */
 class NotificationUrlTest extends BunqSdkTestBase
 {
+    /**
+     * Getter string constants
+     */
+    const GET_PAYMENT = 'getPayment';
+    const GET_BUNQ_ME_TAB = 'getBunqMeTab';
+    const GET_CHAT_MESSAGE = 'getChatMessage';
+    const GET_DRAFT_PAYMENT = 'getDraftPayment';
+
     /**
      * Assertion errors.
      */
@@ -29,6 +39,7 @@ class NotificationUrlTest extends BunqSdkTestBase
     const JSON_PATH_MUTATION_MODEL = self::BASE_PATH_JSON_MODEL . '/Mutation.json';
     const JSON_PATH_BUNQ_ME_TAB_MODEL = self::BASE_PATH_JSON_MODEL . '/BunqMeTab.json';
     const JSON_PATH_CHAT_MESSAGE_MODEL = self::BASE_PATH_JSON_MODEL . '/ChatMessage.json';
+    const JSON_PATH_DRAFT_PAYMENT_MODEL = self::BASE_PATH_JSON_MODEL . '/DraftPayment.json';
 
     /**
      * Model root key.
@@ -37,48 +48,49 @@ class NotificationUrlTest extends BunqSdkTestBase
     const KEY_MUTATION_MODEL = '';
 
     /**
+     * @param string $fileName
+     * @param string $className
+     * @param string $getterName
+     */
+    private function executeTest(string $fileName, string $className, string $getterName)
+    {
+        $jsonString = FileUtil::getFileContents($fileName);
+        $notificationUrl = $this->getNotificationUrlFromJson($jsonString);
+
+        $model = $notificationUrl->getObject()->$getterName();
+        $referencedModel = $notificationUrl->getObject()->getReferencedObject();
+
+        static::assertNotNull($model);
+        static::assertNotNull($referencedModel);
+        static::assertInstanceOf($className, $referencedModel);
+    }
+
+    /**
      */
     public function testMutationModel()
     {
-        $jsonString = FileUtil::getFileContents(self::JSON_PATH_MUTATION_MODEL);
-        $notificationUrl = $this->getNotificationUrlFromJson($jsonString);
-
-        $paymentModel = $notificationUrl->getObject()->getPayment();
-        $referencedPaymentModel = $notificationUrl->getObject()->getReferencedObject();
-
-        $this->assetReferencedModelIsNotNull($paymentModel);
-        $this->assetReferencedModelIsNotNull($referencedPaymentModel);
-        static::assertInstanceOf(Payment::class, $referencedPaymentModel);
+        $this->executeTest(self::JSON_PATH_MUTATION_MODEL, Payment::class, self::GET_PAYMENT);
     }
 
     /**
      */
     public function testBunqMeTabModel()
     {
-        $jsonString = FileUtil::getFileContents(self::JSON_PATH_BUNQ_ME_TAB_MODEL);
-        $notificationUrl = $this->getNotificationUrlFromJson($jsonString);
-
-        $bunqMeTabModel = $notificationUrl->getObject()->getBunqMeTab();
-        $referencedBunqMeTabModel = $notificationUrl->getObject()->getReferencedObject();
-
-        $this->assetReferencedModelIsNotNull($bunqMeTabModel);
-        $this->assetReferencedModelIsNotNull($referencedBunqMeTabModel);
-        static::assertInstanceOf(BunqMeTab::class, $referencedBunqMeTabModel);
+        $this->executeTest(self::JSON_PATH_BUNQ_ME_TAB_MODEL, BunqMeTab::class, self::GET_BUNQ_ME_TAB);
     }
+
+    ///**
+    // */
+    //public function testChatMessageModel()
+    //{
+    //    $this->executeTest(self::JSON_PATH_CHAT_MESSAGE_MODEL, ChatMessage::class, self::GET_CHAT_MESSAGE);
+    //}
 
     /**
      */
-    public function testChatMessageModel()
+    public function testDraftPaymentModel()
     {
-        $jsonString = FileUtil::getFileContents(self::JSON_PATH_CHAT_MESSAGE_MODEL);
-        $notificationUrl = $this->getNotificationUrlFromJson($jsonString);
-
-        $chatMessageModel = $notificationUrl->getObject()->getChatMessage();
-        $referencedChatMessageModel = $notificationUrl->getObject()->getReferencedObject();
-
-        $this->assetReferencedModelIsNotNull($chatMessageModel);
-        $this->assetReferencedModelIsNotNull($referencedChatMessageModel);
-        static::assertInstanceOf(BunqMeTab::class, $referencedChatMessageModel);
+        $this->executeTest(self::JSON_PATH_DRAFT_PAYMENT_MODEL, DraftPayment::class, self::GET_DRAFT_PAYMENT);
     }
 
     /**
@@ -92,16 +104,11 @@ class NotificationUrlTest extends BunqSdkTestBase
         static::assertNotNull($json, self::ASSERT_JSON_DECODE_ERROR);
         $notificationObject = $this->getNotificationObjectAsString($json);
         $notificationUrl = NotificationUrl::fromJsonToModel($notificationObject);
+        $notificationUrl = self::assertInstanceOfNotificationUrl($notificationUrl);
 
-        return self::assertInstanceOfNotificationUrl($notificationUrl);
-    }
+        static::assertNotNull($notificationUrl->getObject());
 
-    /**
-     * @param BunqModel $model
-     */
-    private function assetReferencedModelIsNotNull(BunqModel $model)
-    {
-        static::assertNotNull($model);
+        return $notificationUrl;
     }
 
     /**
