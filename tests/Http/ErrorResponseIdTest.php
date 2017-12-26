1a182a6d4119903618a4b703ad1d41325e4abcd5
Kevin Hellemun <khellemun@bunq.com>
2017-12-26 15:16:46 +0100

    Added test to make sure that the response header can be retrieved from a failed request. (bunq/sdk_php#88)

---
+++ tests/Http/ErrorResponseIdTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace bunq\test\Http;
+
+use bunq\Exception\BadRequestException;
+use bunq\Model\Generated\Endpoint\UserPerson;
+use bunq\test\BunqSdkTestBase;
+
+/**
+ * Tests if the response id from a failed request can be retrieved successfully.
+ */
+class ErrorResponseIdTest extends BunqSdkTestBase
+{
+    /**
+     */
+    public function testBadRequestWitResponseId()
+    {
+        $caughtExcretion = null;
+
+        try {
+            UserPerson::get(static::$apiContext, 0);
+        } catch (BadRequestException $exception) {
+            $caughtExcretion = $exception;
+        }
+
+        static::assertNotNull($caughtExcretion);
+        static::assertNotNull($caughtExcretion->getResponseId());
+    }
+}
