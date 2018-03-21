7a4dc4c05ff48b4bc4290cb3c075f2de2c1a331e
Kevin Hellemun <khellemun@bunq.com>
2018-03-21 09:55:09 +0100

    Renamed filename accordingly.

---
+++ src/Context/BunqContext.php
@@ -0,0 +1,65 @@
+<?php
+namespace bunq\Context;
+
+use bunq\Exception\BunqException;
+
+class BunqContext
+{
+    /**
+     * Error constants.
+     */
+    const ERROR_API_CONTEXT_HAS_NOT_BEEN_LOADED = 'apiContext has not been loaded.';
+    const ERROR_USER_CONTEXT_NOT_LOADED = 'userContext has not been loaded, you can load this by loading apiContext.';
+
+    /**
+     * @var ApiContext
+     */
+    protected static $apiContext;
+
+    /**
+     * @var UserContext
+     */
+    protected static $userContext;
+
+    /**
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @param ApiContext $apiContext
+     */
+    public static function loadApiContext(ApiContext $apiContext)
+    {
+        static::$apiContext = $apiContext;
+        static::$userContext = new UserContext($apiContext->getSessionContext()->getUserId());
+        static::$userContext->initMainMonetaryAccount();
+    }
+
+    /**
+     * @return ApiContext
+     * @throws BunqException
+     */
+    public static function getApiContext(): ApiContext
+    {
+        if (is_null(static::$apiContext)) {
+            throw new BunqException(self::ERROR_API_CONTEXT_HAS_NOT_BEEN_LOADED);
+        } else {
+            return static::$apiContext;
+        }
+    }
+
+    /**
+     * @return UserContext
+     * @throws BunqException
+     */
+    public static function getUserContext(): UserContext
+    {
+        if (is_null(static::$userContext)) {
+            throw new BunqException(self::ERROR_USER_CONTEXT_NOT_LOADED);
+        } else {
+            return static::$userContext;
+        }
+    }
+}
