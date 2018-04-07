440a99275dc2495edc257377b7c2e3917a69fee7
Kevin Hellemun <khellemun@bunq.com>
2018-04-07 10:40:27 +0200

    Added test for auto update on BunqContext. (bunq/sdk_php#94)

--- tests/Context/ApiContextTest.php
+++ tests/Context/ApiContextTest.php
@@ -3,7 +3,9 @@ namespace bunq\test\Context;
 
 use bunq\Context\ApiContext;
 use bunq\Context\BunqContext;
+use bunq\Context\SessionContext;
 use bunq\test\BunqSdkTestBase;
+use DateTime;
 
 /**
  * @author Daniil Belyakov <daniil@bunq.com>
@@ -15,6 +17,10 @@ class ApiContextTest extends BunqSdkTestBase
      * Path to a temporary context file.
      */
     const CONTEXT_FILE_PATH_TEST = __DIR__ . '/context-save-restore-test.conf';
+
+    /**
+     */
+    const DATE_TIME_INTERVAL_ONE_YEAR = 'P1Y';
 
     /**
      */
@@ -35,5 +41,37 @@ class ApiContextTest extends BunqSdkTestBase
         $apiContextRestored = ApiContext::restore(self::CONTEXT_FILE_PATH_TEST);
 
         static::assertEquals($apiContextJson, $apiContextRestored->toJson());
+    }
+
+    /**
+     */
+    public function testAutoUpdateBunqContext()
+    {
+        $apiContext = BunqContext::getApiContext();
+
+        $contextJson = json_decode($apiContext->toJson(), true);
+        $expireTime =
+            DateTime::createFromFormat(
+                SessionContext::FORMAT_MICROTIME,
+                $contextJson[ApiContext::FIELD_SESSION_CONTEXT][SessionContext::FIELD_EXPIRY_TIME]
+            );
+        $expireTime->sub(new \DateInterval(self::DATE_TIME_INTERVAL_ONE_YEAR));
+        $contextJson[ApiContext::FIELD_SESSION_CONTEXT][SessionContext::FIELD_EXPIRY_TIME] =
+            $expireTime->format(SessionContext::FORMAT_MICROTIME);
+
+        $expiredApiContext = ApiContext::fromJson(json_encode($contextJson));
+        $validApiContext = clone $expiredApiContext;
+
+        static::assertTrue($validApiContext->ensureSessionActive());
+        static::assertNotEquals($expiredApiContext, $validApiContext);
+
+        BunqContext::updateApiContext(clone $expiredApiContext);
+
+        static::assertEquals($expiredApiContext, BunqContext::getApiContext());
+
+        BunqContext::getUserContext()->refreshUserContext();
+
+        static::assertNotEquals($expiredApiContext, BunqContext::getApiContext());
+        static::assertFalse(BunqContext::getApiContext()->ensureSessionActive());
     }
 }
