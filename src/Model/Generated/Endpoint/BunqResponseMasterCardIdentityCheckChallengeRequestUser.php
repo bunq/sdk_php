594da583b0f65d430c1333f51660e44c4f4a750a
bunq <github@bunq.com>
2022-07-12 14:26:40 +0000

    Regenerated code based on the latest tag

---
+++ src/Model/Generated/Endpoint/BunqResponseMasterCardIdentityCheckChallengeRequestUser.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseMasterCardIdentityCheckChallengeRequestUser extends BunqResponse
+{
+    /**
+     * @return MasterCardIdentityCheckChallengeRequestUser
+     */
+    public function getValue(): MasterCardIdentityCheckChallengeRequestUser
+    {
+        return parent::getValue();
+    }
+}
