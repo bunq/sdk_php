d834d80bfa5ef5b144f35f92fba80aa58501d1ca
bunq <github@bunq.com>
2022-08-03 15:29:56 +0000

    Regenerated code based on the latest tag

---
+++ src/Model/Generated/Endpoint/BunqResponseNotificationFilterGroupList.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseNotificationFilterGroupList extends BunqResponse
+{
+    /**
+     * @return NotificationFilterGroup[]
+     */
+    public function getValue(): array
+    {
+        return parent::getValue();
+    }
+}
