4d1b102bd7683b42f4119b93dda4a73f1fa229b1
bunq <github@bunq.com>
2022-09-28 12:04:04 +0000

    Regenerated code based on the latest tag

---
+++ src/Model/Generated/Endpoint/BunqResponseNotificationFilterEmailUser.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseNotificationFilterEmailUser extends BunqResponse
+{
+    /**
+     * @return NotificationFilterEmailUser
+     */
+    public function getValue(): NotificationFilterEmailUser
+    {
+        return parent::getValue();
+    }
+}
