89babee1d3cf6cba614c2c2a24f98bbcaf08f868
bunq <github@bunq.com>
2022-09-30 10:19:36 +0000

    Regenerated code based on the latest tag

---
+++ src/Model/Generated/Endpoint/BunqResponseNotificationFilterPush.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseNotificationFilterPush extends BunqResponse
+{
+    /**
+     * @return NotificationFilterPush
+     */
+    public function getValue(): NotificationFilterPush
+    {
+        return parent::getValue();
+    }
+}
