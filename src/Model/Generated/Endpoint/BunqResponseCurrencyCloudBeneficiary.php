b4001803e573fdb210cda52ab16ec17d834b69fa
bunq <github@bunq.com>
2022-11-02 11:14:34 +0000

    Regenerated code based on the latest tag

---
+++ src/Model/Generated/Endpoint/BunqResponseCurrencyCloudBeneficiary.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseCurrencyCloudBeneficiary extends BunqResponse
+{
+    /**
+     * @return CurrencyCloudBeneficiary
+     */
+    public function getValue(): CurrencyCloudBeneficiary
+    {
+        return parent::getValue();
+    }
+}
---
+++ src/Model/Generated/Endpoint/BunqResponseCurrencyCloudBeneficiaryRequirementList.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseCurrencyCloudBeneficiaryRequirementList extends BunqResponse
+{
+    /**
+     * @return CurrencyCloudBeneficiaryRequirement[]
+     */
+    public function getValue(): array
+    {
+        return parent::getValue();
+    }
+}
