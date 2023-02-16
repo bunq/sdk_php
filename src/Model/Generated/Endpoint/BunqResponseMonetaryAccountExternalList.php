e6c14c07920233ef223f9a1a1e67e15c2c200331
bunq <github@bunq.com>
2023-02-16 15:29:06 +0000

    Regenerated code based on the latest tag

---
+++ src/Model/Generated/Endpoint/BunqResponseMonetaryAccountExternalList.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseMonetaryAccountExternalList extends BunqResponse
+{
+    /**
+     * @return MonetaryAccountExternal[]
+     */
+    public function getValue(): array
+    {
+        return parent::getValue();
+    }
+}
