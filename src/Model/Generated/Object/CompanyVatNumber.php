d31887780d1ed747c3e44aa0b62d3f659e8ae3c9
bunq <github@bunq.com>
2023-07-17 13:29:36 +0000

    Regenerated code based on the latest tag

--- src/Model/Generated/Object/CompanyVatNumber.php
+++ src/Model/Generated/Object/CompanyVatNumber.php
@@ -9,6 +9,13 @@ use bunq\Model\Core\BunqModel;
 class CompanyVatNumber extends BunqModel
 {
     /**
+     * The VAT identification number type.
+     *
+     * @var string|null
+     */
+    protected $type;
+
+    /**
      * The country of the VAT identification number.
      *
      * @var string
@@ -53,6 +60,27 @@ class CompanyVatNumber extends BunqModel
         $this->typeFieldForRequest = $type;
         $this->countryFieldForRequest = $country;
         $this->valueFieldForRequest = $value;
+    }
+
+    /**
+     * The VAT identification number type.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @deprecated User should not be able to set values via setters, use
+     * constructor.
+     *
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
     }
 
     /**
@@ -102,6 +130,10 @@ class CompanyVatNumber extends BunqModel
      */
     public function isAllFieldNull()
     {
+        if (!is_null($this->type)) {
+            return false;
+        }
+
         if (!is_null($this->country)) {
             return false;
         }
