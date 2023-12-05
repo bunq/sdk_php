6c71ff10fe945806394585a5fe07464a088817ff
bunq <github@bunq.com>
2023-12-05 10:04:24 +0000

    Regenerated code based on the latest tag

---
+++ src/Model/Generated/Endpoint/BunqResponseNoteTextAdyenCardTransaction.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseNoteTextAdyenCardTransaction extends BunqResponse
+{
+    /**
+     * @return NoteTextAdyenCardTransaction
+     */
+    public function getValue(): NoteTextAdyenCardTransaction
+    {
+        return parent::getValue();
+    }
+}
---
+++ src/Model/Generated/Endpoint/BunqResponseNoteTextAdyenCardTransactionList.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseNoteTextAdyenCardTransactionList extends BunqResponse
+{
+    /**
+     * @return NoteTextAdyenCardTransaction[]
+     */
+    public function getValue(): array
+    {
+        return parent::getValue();
+    }
+}
