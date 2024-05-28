547b9176ca5b88a800e1748a7e568afbd27bf62f
bunq <github@bunq.com>
2024-05-28 21:03:28 +0000

    Regenerated code based on the latest tag

---
+++ src/Model/Generated/Endpoint/BunqResponseNoteTextPaymentDelayedList.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseNoteTextPaymentDelayedList extends BunqResponse
+{
+    /**
+     * @return NoteTextPaymentDelayed[]
+     */
+    public function getValue(): array
+    {
+        return parent::getValue();
+    }
+}
---
+++ src/Model/Generated/Endpoint/NoteTextPaymentDelayed.php
@@ -0,0 +1,345 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Context\ApiContext;
+use bunq\Http\ApiClient;
+use bunq\Http\BunqResponse;
+use bunq\Model\Core\BunqModel;
+use bunq\Model\Generated\Object\LabelUser;
+
+/**
+ * Used to manage text notes.
+ *
+ * @generated
+ */
+class NoteTextPaymentDelayed extends BunqModel
+{
+    /**
+     * Endpoint constants.
+     */
+    const ENDPOINT_URL_CREATE = 'user/%s/monetary-account/%s/payment-delayed/%s/note-text';
+    const ENDPOINT_URL_UPDATE = 'user/%s/monetary-account/%s/payment-delayed/%s/note-text/%s';
+    const ENDPOINT_URL_DELETE = 'user/%s/monetary-account/%s/payment-delayed/%s/note-text/%s';
+    const ENDPOINT_URL_LISTING = 'user/%s/monetary-account/%s/payment-delayed/%s/note-text';
+    const ENDPOINT_URL_READ = 'user/%s/monetary-account/%s/payment-delayed/%s/note-text/%s';
+
+    /**
+     * Field constants.
+     */
+    const FIELD_CONTENT = 'content';
+
+    /**
+     * Object type.
+     */
+    const OBJECT_TYPE_GET = 'NoteText';
+
+    /**
+     * The id of the note.
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * The timestamp of the note's creation.
+     *
+     * @var string
+     */
+    protected $created;
+
+    /**
+     * The timestamp of the note's last update.
+     *
+     * @var string
+     */
+    protected $updated;
+
+    /**
+     * The label of the user who created this note.
+     *
+     * @var LabelUser
+     */
+    protected $labelUserCreator;
+
+    /**
+     * The content of the note.
+     *
+     * @var string
+     */
+    protected $content;
+
+    /**
+     * The content of the note.
+     *
+     * @var string|null
+     */
+    protected $contentFieldForRequest;
+
+    /**
+     * @param string|null $content The content of the note.
+     */
+    public function __construct(string  $content = null)
+    {
+        $this->contentFieldForRequest = $content;
+    }
+
+    /**
+     * @param int $paymentDelayedId
+     * @param int|null $monetaryAccountId
+     * @param string|null $content The content of the note.
+     * @param string[] $customHeaders
+     *
+     * @return BunqResponseInt
+     */
+    public static function create(int $paymentDelayedId, int $monetaryAccountId = null, string  $content = null, array $customHeaders = []): BunqResponseInt
+    {
+        $apiClient = new ApiClient(static::getApiContext());
+        $responseRaw = $apiClient->post(
+            vsprintf(
+                self::ENDPOINT_URL_CREATE,
+                [static::determineUserId(), static::determineMonetaryAccountId($monetaryAccountId), $paymentDelayedId]
+            ),
+            [self::FIELD_CONTENT => $content],
+            $customHeaders
+        );
+
+        return BunqResponseInt::castFromBunqResponse(
+            static::processForId($responseRaw)
+        );
+    }
+
+    /**
+     * @param int $paymentDelayedId
+     * @param int $noteTextPaymentDelayedId
+     * @param int|null $monetaryAccountId
+     * @param string|null $content The content of the note.
+     * @param string[] $customHeaders
+     *
+     * @return BunqResponseInt
+     */
+    public static function update(int $paymentDelayedId, int $noteTextPaymentDelayedId, int $monetaryAccountId = null, string  $content = null, array $customHeaders = []): BunqResponseInt
+    {
+        $apiClient = new ApiClient(static::getApiContext());
+        $responseRaw = $apiClient->put(
+            vsprintf(
+                self::ENDPOINT_URL_UPDATE,
+                [static::determineUserId(), static::determineMonetaryAccountId($monetaryAccountId), $paymentDelayedId, $noteTextPaymentDelayedId]
+            ),
+            [self::FIELD_CONTENT => $content],
+            $customHeaders
+        );
+
+        return BunqResponseInt::castFromBunqResponse(
+            static::processForId($responseRaw)
+        );
+    }
+
+    /**
+     * @param string[] $customHeaders
+     * @param int $paymentDelayedId
+     * @param int $noteTextPaymentDelayedId
+     *
+     * @return BunqResponseNull
+     */
+    public static function delete(int $paymentDelayedId, int $noteTextPaymentDelayedId, int $monetaryAccountId = null, array $customHeaders = []): BunqResponseNull
+    {
+        $apiClient = new ApiClient(static::getApiContext());
+        $responseRaw = $apiClient->delete(
+            vsprintf(
+                self::ENDPOINT_URL_DELETE,
+                [static::determineUserId(), static::determineMonetaryAccountId($monetaryAccountId), $paymentDelayedId, $noteTextPaymentDelayedId]
+            ),
+            $customHeaders
+        );
+
+        return BunqResponseNull::castFromBunqResponse(
+            new BunqResponse(null, $responseRaw->getHeaders())
+        );
+    }
+
+    /**
+     * This method is called "listing" because "list" is a restricted PHP word
+     * and cannot be used as constants, class names, function or method names.
+     *
+     * @param int $paymentDelayedId
+     * @param int|null $monetaryAccountId
+     * @param string[] $params
+     * @param string[] $customHeaders
+     *
+     * @return BunqResponseNoteTextPaymentDelayedList
+     */
+    public static function listing(int $paymentDelayedId, int $monetaryAccountId = null, array $params = [], array $customHeaders = []): BunqResponseNoteTextPaymentDelayedList
+    {
+        $apiClient = new ApiClient(static::getApiContext());
+        $responseRaw = $apiClient->get(
+            vsprintf(
+                self::ENDPOINT_URL_LISTING,
+                [static::determineUserId(), static::determineMonetaryAccountId($monetaryAccountId), $paymentDelayedId]
+            ),
+            $params,
+            $customHeaders
+        );
+
+        return BunqResponseNoteTextPaymentDelayedList::castFromBunqResponse(
+            static::fromJsonList($responseRaw, self::OBJECT_TYPE_GET)
+        );
+    }
+
+    /**
+     * @param int $paymentDelayedId
+     * @param int $noteTextPaymentDelayedId
+     * @param int|null $monetaryAccountId
+     * @param string[] $customHeaders
+     *
+     * @return BunqResponseNoteTextPaymentDelayed
+     */
+    public static function get(int $paymentDelayedId, int $noteTextPaymentDelayedId, int $monetaryAccountId = null, array $customHeaders = []): BunqResponseNoteTextPaymentDelayed
+    {
+        $apiClient = new ApiClient(static::getApiContext());
+        $responseRaw = $apiClient->get(
+            vsprintf(
+                self::ENDPOINT_URL_READ,
+                [static::determineUserId(), static::determineMonetaryAccountId($monetaryAccountId), $paymentDelayedId, $noteTextPaymentDelayedId]
+            ),
+            [],
+            $customHeaders
+        );
+
+        return BunqResponseNoteTextPaymentDelayed::castFromBunqResponse(
+            static::fromJson($responseRaw, self::OBJECT_TYPE_GET)
+        );
+    }
+
+    /**
+     * The id of the note.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @deprecated User should not be able to set values via setters, use
+     * constructor.
+     *
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * The timestamp of the note's creation.
+     *
+     * @return string
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @deprecated User should not be able to set values via setters, use
+     * constructor.
+     *
+     * @param string $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * The timestamp of the note's last update.
+     *
+     * @return string
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * @deprecated User should not be able to set values via setters, use
+     * constructor.
+     *
+     * @param string $updated
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+
+    /**
+     * The label of the user who created this note.
+     *
+     * @return LabelUser
+     */
+    public function getLabelUserCreator()
+    {
+        return $this->labelUserCreator;
+    }
+
+    /**
+     * @deprecated User should not be able to set values via setters, use
+     * constructor.
+     *
+     * @param LabelUser $labelUserCreator
+     */
+    public function setLabelUserCreator($labelUserCreator)
+    {
+        $this->labelUserCreator = $labelUserCreator;
+    }
+
+    /**
+     * The content of the note.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @deprecated User should not be able to set values via setters, use
+     * constructor.
+     *
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAllFieldNull()
+    {
+        if (!is_null($this->id)) {
+            return false;
+        }
+
+        if (!is_null($this->created)) {
+            return false;
+        }
+
+        if (!is_null($this->updated)) {
+            return false;
+        }
+
+        if (!is_null($this->labelUserCreator)) {
+            return false;
+        }
+
+        if (!is_null($this->content)) {
+            return false;
+        }
+
+        return true;
+    }
+}
